2c310e7f21d1777a0a6af769db80b3edebebb270
Mugisha Moses <mossplix@gmail.com>
2013-02-14 18:05:18 +0300

    rm stuff that was to be git ignored

--- .dist-buildwrapper/app/Test.hs
+++
@@ -1,8 +0,0 @@
-module Test where
-   fibTuple :: (Integer, Integer, Integer) -> (Integer, Integer, Integer)
-   fibTuple (a,b,0) = (a,b,0)
-   fibTuple(a,b,index)=fibTuple(b,a+b,index-1)
-   
-   
-   
-        
